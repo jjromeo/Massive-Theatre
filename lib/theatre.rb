096a22fa7d6d0f4b7091470bd9dfe5ae6bda1c11
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-04 14:29:37 +0000

    added test for theatre making booking - in the midst of finishing method for taking in booking

--- lib/theatre.rb
+++ lib/theatre.rb
@@ -4,5 +4,18 @@ class Theatre
         @rows = []
         100.times {@rows << Row.new}
     end
+
+    def make_booking(booking_request)
+        booking_request.gsub!(/[()]/, "")
+        arr = booking_request.split(",")
+        first_row_and_seat = rows[arr[1]].split(":")
+        last_row_and_seat = rows[arr[2]].split(":")
+        if first_row_and_seat[0] == last_row_and_seat[0]
+           booking_range = (first_row_and_seat[1].to_i..last_row_and_seat[1].to_i)
+           booking_range.each {|seat| first_row_and_seat[0].book_seat!(seat)} 
+        else
+            raise "Bookings must stay on the same row!"
+        end
+    end
 end
 
--- spec/theatre_spec.rb
+++ spec/theatre_spec.rb
@@ -2,18 +2,25 @@ require 'spec_helper'
 
 describe Theatre do
 
+    let(:theatre) {Theatre.new}
+
     context 'theatre basics' do 
 
         it 'has 100 rows' do 
-            theatre = Theatre.new
             expect(theatre.rows.count).to eq 100
         end
 
        it 'each row has 50 seats' do 
-            theatre = Theatre.new
             expect(theatre.rows.all? {|row| row.seats.count == 50}).to eq true
         end
 
+    end
+
+    context 'taking bookings' do 
+        it 'can accept a group booking' do 
+            expect(theatre.rows[67].seats[44..48]).to receive(:book!)
+            theatre.make_booking("(1,67:44,67:47)")       
+        end
     end
 
 end
