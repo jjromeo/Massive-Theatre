888d39b28e27bd3e02397100e315627d788552bd
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-06 16:01:12 +0000

    booking handler can make bookings based on those it takes, and can tell which were rejected and why

--- lib/booking_handler.rb
+++ lib/booking_handler.rb
@@ -1,8 +1,10 @@
 class BookingHandler 
-    attr_reader :parsed_bookings
+    attr_reader :parsed_bookings, :theatre, :rejected_bookings
     
     def initialize
        @parsed_bookings = [] 
+       @theatre = Theatre.new
+       @rejected_bookings = []
     end
 
     
@@ -11,5 +13,15 @@ class BookingHandler
         f = File.open(file, "r")
         f.each_line {|line| @parsed_bookings << "#{line.slice(0..-4)}"}
         f.close
+    end
+
+    def book_requests
+        parsed_bookings.each_with_index do |request, index|
+            begin
+                theatre.make_booking(request)
+            rescue RuntimeError => e
+                rejected_bookings << "Booking number #{index}: #{e.message}" 
+            end
+        end
     end
 end
--- spec/booking_handler_spec.rb
+++ spec/booking_handler_spec.rb
@@ -2,9 +2,33 @@ require 'spec_helper'
 
 describe BookingHandler do
 
+    let(:booker) {BookingHandler.new}
+
     it 'can read the contents of a file' do 
-        booker = BookingHandler.new
         booker.parse_bookings('./booking_requests.txt')
         expect(booker.parsed_bookings.count).to eq 500
+    end
+
+    it 'can book places based on the contents of the file it takes in' do 
+        booker.parse_bookings('./booking_requests.txt')
+        expect(booker.theatre).to receive(:make_booking).exactly(500).times
+        booker.book_requests
+    end
+
+    context 'when bookings have been taken and made' do 
+
+        before do 
+            booker.parse_bookings('./booking_requests.txt')
+            booker.book_requests
+        end
+
+        it 'knows how many bookings have been rejected' do 
+            expect(booker.rejected_bookings.count).to eq(11)
+        end
+
+        it 'knows why each booking has been rejected and which one' do 
+            expect(booker.rejected_bookings.first).to eq "Booking number 32: Sorry, 5 is the maximum number of bookings"
+        end
+
     end
 end
