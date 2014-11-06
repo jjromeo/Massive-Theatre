55e5aea4b4595fc8a8bad356d5b732175466fe51
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-06 15:08:47 +0000

    started adding testable way to read bookings from file

---
+++ lib/booking_handler.rb
@@ -0,0 +1,12 @@
+class BookingHandler 
+    attr_reader :parsed_bookings_count
+    
+    def initialize
+       @parsed_bookings_count = 0 
+    end
+
+    
+
+    def parse_bookings(file)
+    end
+end
---
+++ spec/booking_handler_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe BookingHandler do
+
+    it 'can read the contents of a file' do 
+        booker = BookingHandler.new
+        booker.parse_bookings('../booking_requests.txt')
+        expect(booker.parsed_bookings_count).to eq 500
+    end
+end
