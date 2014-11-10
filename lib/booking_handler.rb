81f960d337036934368fc9b190d25e42e6271ff9
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-10 13:03:17 +0000

    down to only 3 code smells

--- lib/booking_handler.rb
+++ lib/booking_handler.rb
@@ -2,9 +2,11 @@ class BookingHandler
     attr_reader :parsed_bookings, :theatre, :rejected_bookings, :booked_seats, :available_seats
     
     def initialize
-       @parsed_bookings = [] 
-       @theatre = Theatre.new
-       @rejected_bookings = []
+        @parsed_bookings = [] 
+        @theatre = Theatre.new
+        @rejected_bookings = []
+        @available_places = []
+        @booked_places = []
     end
 
     def process_bookings(file)
@@ -13,35 +15,31 @@ class BookingHandler
     end
 
     def parse_bookings(file)
-        f = File.open(file, "r")
-        f.each_line {|line| @parsed_bookings << "#{line.slice(0..-4)}"}
-        f.close
+        file = File.open(file, "r")
+        file.each_line {|line| @parsed_bookings << "#{line.slice(0..-4)}"}
+        file.close
     end
 
     def book_requests
         parsed_bookings.each_with_index do |request, index|
             begin
                 theatre.make_booking(request)
-            rescue RuntimeError => e
-                rejected_bookings << "Booking number #{index}: #{e.message}" 
+            rescue RuntimeError => error
+                rejected_bookings << "Booking number #{index}: #{error.message}" 
             end
         end
     end
 
     def booked_seats
-        @booked_places = []
         theatre.rows.each do |row|
-            booked =  row.seats.select {|seat| seat.booked?}
-            booked.each { |booked_seat| @booked_places << booked_seat}
+            row.seats.select {|seat| seat.booked?}.each { |booked_seat| @booked_places << booked_seat}
         end
             @booked_places
     end
     
     def available_seats
-        @available_places = []
         theatre.rows.each do |row|
-            available = row.seats.reject {|seat| seat.booked?}
-            available.each { |available_seat| @available_places << available_seat}
+            row.seats.reject {|seat| seat.booked?}.each { |available_seat| @available_places << available_seat}
         end
             @available_places
     end
