e8438a1178e00d3042b208f8280112e8423ccdbc
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-13 14:04:54 +0000

    added check_gap method to lower code duplication issue from code climate

--- lib/row.rb
+++ lib/row.rb
@@ -12,29 +12,35 @@ class Row
 
     private
     def check_for_gaps(seat_number)
-        behind, in_front = seats[seat_number - 2], seats[seat_number + 2]
-        if behind && in_front 
+        two_behind, behind, two_in_front, in_front = seats[seat_number - 2], seats[seat_number - 1], seats[seat_number + 2], seats[seat_number + 1]
+        if two_behind && two_in_front 
             check_both(seat_number)
-        elsif behind 
-            check_gap_behind(seat_number)
-        elsif in_front 
-            check_gap_in_front(seat_number)
+        elsif two_behind 
+            check_gap(two_behind, behind)
+        elsif two_in_front 
+            check_gap(two_in_front, in_front)
         end
     end
     
-    def check_gap_behind(seat_number)
+    def check_gap_two_behind(seat_number)
         if seats[seat_number - 2].booked? && !(seats[seat_number - 1].booked?)
             gap_warning
         end
     end
 
+    def check_gap(seat_two_away, seat_one_away)
+        if seat_two_away.booked? && !(seat_one_away.booked?)
+            gap_warning
+        end
+    end
+
     def check_both(seat_number)
-            check_gap_behind(seat_number)
-            check_gap_in_front(seat_number)
+            check_gap_two_behind(seat_number)
+            check_gap_two_in_front(seat_number)
     end
 
 
-    def check_gap_in_front(seat_number)
+    def check_gap_two_in_front(seat_number)
         if seats[seat_number + 2].booked? && !(seats[seat_number + 1].booked?)
             gap_warning
         end
