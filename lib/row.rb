4a4c1212c4b3a9b24c5e3b8e69cc96d097eaba4f
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-13 15:36:20 +0000

    did the best re-factor ever getting rid of about 10 lines with a change in control flow

--- lib/row.rb
+++ lib/row.rb
@@ -13,20 +13,10 @@ class Row
     private
     def check_for_gaps(seat_number)
         two_behind, behind, two_in_front, in_front = seats[seat_number - 2], seats[seat_number - 1], seats[seat_number + 2], seats[seat_number + 1]
-        if two_behind && two_in_front 
-            check_both(seat_number)
-        elsif two_behind 
-            check_gap(two_behind, behind)
-        elsif two_in_front 
-            check_gap(two_in_front, in_front)
-        end
+            check_gap(two_behind, behind) unless !two_behind
+            check_gap(two_in_front, in_front) unless !two_in_front
     end
     
-    def check_gap_two_behind(seat_number)
-        if seats[seat_number - 2].booked? && !(seats[seat_number - 1].booked?)
-            gap_warning
-        end
-    end
 
     def check_gap(seat_two_away, seat_one_away)
         if seat_two_away.booked? && !(seat_one_away.booked?)
@@ -34,18 +24,6 @@ class Row
         end
     end
 
-    def check_both(seat_number)
-            check_gap_two_behind(seat_number)
-            check_gap_two_in_front(seat_number)
-    end
-
-
-    def check_gap_two_in_front(seat_number)
-        if seats[seat_number + 2].booked? && !(seats[seat_number + 1].booked?)
-            gap_warning
-        end
-    end
-    
     def gap_warning
         raise "Sorry you cannot book if it will leave a gap of 1!"
     end
